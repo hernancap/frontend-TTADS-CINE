0f7f0aae3faada74bda504bc102b65b7a273f75c
Hernan <hernancaparros@gmail.com>
2025-04-21 08:38:05 -0300

    Modificacion de variables de entorno

--- src/components/PeliculasHome/MovieCard.tsx
+++ src/components/PeliculasHome/MovieCard.tsx
@@ -1,5 +1,6 @@
 import { Pelicula } from '../../types';
 import { Link } from 'react-router-dom';
+const API = import.meta.env.VITE_API_BASE_URL.replace('/api','')
 
 interface Props {
   pelicula: Pelicula;
@@ -10,7 +11,7 @@ const MovieCard = ({ pelicula }: Props) => {
     <Link to={`/pelicula/${pelicula.id}`} className="movie-card-link">
       <div className="movie-card">
         <img 
-          src={pelicula.poster_path ? `http://localhost:3000/uploads/${pelicula.poster_path}` : 'src/assets/default_poster.jpeg' } 
+          src={pelicula.poster_path ? `${API}/uploads/${pelicula.poster_path}` : 'src/assets/default_poster.jpeg' } 
           alt={pelicula.nombre}
           className="movie-poster"
         />
